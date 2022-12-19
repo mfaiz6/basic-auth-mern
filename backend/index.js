e4f7936a2b1ca8224098ce6174ea5439f76f82a5
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-19 16:07:05 +0530

    add register functionality

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+app.use(cors())
+app.use(express.json())
 const port = 1337
+const UserAuth = require("./user.model")
+
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_URI)
+.then(console.log("Connected to MongoDB"))
+.catch((err => console.log(err)))
 
 
-app.post("/api/register", (req, res) => {
-    console.log(req.body.name)
+app.post("/api/register", async (req, res) => {
+    try {
+        const newUser = await UserAuth.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.status(200).json(newUser)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 
